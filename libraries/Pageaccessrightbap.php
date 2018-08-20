903ed1e66790a64f16ee9403835b7d90fb6feb2f
Basit Adhi Prabowo <basit@unisayogya.ac.id>
2018-08-20 18:11:31 +0700

    Update Pageaccessrightbap.php

--- libraries/Pageaccessrightbap.php
+++ libraries/Pageaccessrightbap.php
@@ -71,6 +71,6 @@ class Pageaccessrightbap
      */
     function checkAccessRight($page_id)
     {
-        return in_array($page_id, (array) json_decode(ifnull($this->CI->session->userdata("PageAccessRightBAP"), array())));
+        return in_array($page_id, (array) json_decode(ifnull($this->CI->session->userdata("PageAccessRightBAP"), "[]")));
     }
 }
